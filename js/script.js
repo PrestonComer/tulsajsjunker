ded48d4e371916541a8c701035eb665e05785f1f
PrestonC <1123preston1123@gmail.com>
2023-01-27 01:42:40 -0600

    changed styling and commenting. testing jsdocs

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
+/**
+ * Iterates through the main pages and hide them all
+ */
 function hideAllPages() {
     $(".mainPage").each(function() {
         $(this).hide();
     });
 }
 
-$('a').on("click", function() {
+/**
+ * Attach the click event to navigation links to hide all
+ * pages then show the one that matches the link pressed
+ */
+$('aside.sidebar__sidebar > a').on("click", function() {
     hideAllPages();
-
     // show the page of the link clicked on
     $(this.dataset.target).show();
 })
